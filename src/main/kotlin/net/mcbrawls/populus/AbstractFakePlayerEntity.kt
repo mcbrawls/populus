6f83a45aaf304ddbc6114a746aeac2c8deb30a45
Daniel Orr <orrdaniel73@gmail.com>
2025-08-14 16:38:06 +0100

    feat: skins, model parts, name tags

--- src/main/kotlin/net/mcbrawls/populus/AbstractFakePlayerEntity.kt
+++ src/main/kotlin/net/mcbrawls/populus/AbstractFakePlayerEntity.kt
@@ -1,27 +1,98 @@
 package net.mcbrawls.populus
 
 import com.mojang.authlib.GameProfile
+import com.mojang.authlib.properties.Property
 import eu.pb4.polymer.core.api.entity.PolymerEntity
+import eu.pb4.polymer.virtualentity.api.ElementHolder
+import eu.pb4.polymer.virtualentity.api.attachment.EntityAttachment
+import eu.pb4.polymer.virtualentity.api.elements.TextDisplayElement
 import net.minecraft.entity.EntityType
+import net.minecraft.entity.data.DataTracker
+import net.minecraft.entity.decoration.DisplayEntity
 import net.minecraft.entity.mob.PathAwareEntity
+import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.packet.Packet
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket
 import net.minecraft.network.packet.s2c.play.PlayerRemoveS2CPacket
 import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
 import net.minecraft.util.Arm
+import net.minecraft.util.math.AffineTransformation
 import net.minecraft.world.GameMode
 import net.minecraft.world.World
+import org.joml.Vector3f
 import xyz.nucleoid.packettweaker.PacketContext
 import java.util.EnumSet
 import java.util.function.Consumer
 
 abstract class AbstractFakePlayerEntity(type: EntityType<out AbstractFakePlayerEntity>, world: World) : PathAwareEntity(type, world), PolymerEntity {
-    open fun createSpawnEntry(): PlayerListS2CPacket.Entry {
-        val uuid = uuid
-        val defaultName = uuid.toString().substring(0..<16)
+    /**
+     * The raw skin texture data for this player's skin.
+     */
+    open val skinData: SkinData? = null
+
+    /**
+     * A byte mask of the model parts to display. (See Skin Customization)
+     */
+    open val modelParts: Byte = Byte.MAX_VALUE
+
+    /**
+     * The base name tag offset from the passenger position.
+     */
+    open val baseNameTagOffset: Float = 0.2f
+
+    /**
+     * The name tag offset for additional lines.
+     */
+    open val additionalNameTagOffset: Float = 0.25f
+
+    /**
+     * The generated profile name for this fake player.
+     */
+    val defaultProfileName: String by lazy { uuid.toString().substring(0..<16) }
+
+    init {
+        val elements = createNameAttachmentElement()
+
+        val holder = ElementHolder()
+        elements.forEach(holder::addPassengerElement)
+        EntityAttachment.ofTicking(holder, this)
+    }
+
+    /**
+     * Creates the text displayed above the player's head.
+     * @return a list of text components to be displayed, first highest
+     */
+    open fun createDisplayNameText(): List<Text> {
+        return emptyList()
+    }
+
+    /**
+     * Creates the attached elements for the player's name tag.
+     * @return null to use default name tag
+     */
+    open fun createNameAttachmentElement(): List<TextDisplayElement> {
+        val components = createDisplayNameText()
+        return components.reversed().mapIndexed { i, text ->
+            val element = TextDisplayElement(text)
+
+            val transformation = AffineTransformation(Vector3f(0.0f, baseNameTagOffset + (additionalNameTagOffset * i), 0.0f), null, null, null)
+            element.setTransformation(transformation)
+
+            element.billboardMode = DisplayEntity.BillboardMode.CENTER
+            element.teleportDuration = 1
+
+            element
+        }
+    }
+
+    /**
+     * Creates the player list entry for this player. Necessary for the player entity to spawn.
+     */
+    open fun createPlayerListEntry(): PlayerListS2CPacket.Entry {
         return PlayerListS2CPacket.Entry(
             uuid,
-            GameProfile(uuid, defaultName),
+            createProfile(),
             false, // listed
             0, // latency
             GameMode.DEFAULT,
@@ -32,16 +103,45 @@ abstract class AbstractFakePlayerEntity(type: EntityType<out AbstractFakePlayerE
         )
     }
 
+    /**
+     * Creates the profile name for this player.
+     * Will be displayed in tab completion & the social interaction menu.
+     */
+    open fun createProfileName(): String {
+        return defaultProfileName
+    }
+
+    /**
+     * Creates the complete profile for this player.
+     */
+    open fun createProfile(): GameProfile {
+        val name = createProfileName()
+        return GameProfile(uuid, name).apply {
+            skinData?.also { data ->
+                properties.put("textures", Property("textures", data.value, data.signature))
+            }
+        }
+    }
+
     override fun onBeforeSpawnPacket(player: ServerPlayerEntity, consumer: Consumer<Packet<*>>) {
+        // send fake player information
         val packet = PlayerListS2CPacket(EnumSet.of(PlayerListS2CPacket.Action.ADD_PLAYER), emptyList()).apply {
-            val entry = createSpawnEntry()
+            val entry = createPlayerListEntry()
             entries = listOf(entry)
         }
 
         player.networkHandler.sendPacket(packet)
     }
 
+    override fun modifyRawTrackedData(entries: MutableList<DataTracker.SerializedEntry<*>>, player: ServerPlayerEntity, initial: Boolean) {
+        // add model part data
+        if (initial) {
+            entries.add(DataTracker.SerializedEntry.of(PlayerEntity.PLAYER_MODEL_PARTS, modelParts))
+        }
+    }
+
     override fun onStoppedTrackingBy(player: ServerPlayerEntity) {
+        // remove fake player
         val packet = PlayerRemoveS2CPacket(listOf(uuid))
         player.networkHandler.sendPacket(packet)
     }
@@ -53,4 +153,6 @@ abstract class AbstractFakePlayerEntity(type: EntityType<out AbstractFakePlayerE
     override fun getPolymerEntityType(context: PacketContext): EntityType<*> {
         return EntityType.PLAYER
     }
+
+    data class SkinData(val value: String, val signature: String? = null)
 }
--- src/main/kotlin/net/mcbrawls/populus/NpcEntity.kt
+++ src/main/kotlin/net/mcbrawls/populus/NpcEntity.kt
@@ -3,11 +3,51 @@ package net.mcbrawls.populus
 import net.minecraft.entity.EntityType
 import net.minecraft.entity.ai.goal.EscapeDangerGoal
 import net.minecraft.entity.ai.goal.WanderAroundGoal
+import net.minecraft.text.Text
 import net.minecraft.world.World
+import java.util.Base64
 
 class NpcEntity(type: EntityType<NpcEntity>, world: World) : AbstractFakePlayerEntity(type, world) {
+    override val skinData: SkinData = DEFAULT_SKIN
+
     override fun initGoals() {
         goalSelector.add(0, EscapeDangerGoal(this, 0.6))
         goalSelector.add(1, WanderAroundGoal(this, 0.3))
+    }
+
+    override fun createDisplayNameText(): List<Text> {
+        val name = createProfileName()
+        return listOf(Text.literal("Fake ($name)"))
+    }
+
+    companion object {
+        val DEFAULT_SKIN = SkinData(
+            encode(
+                """
+                    {
+                      "timestamp" : 1755182191153,
+                      "profileId" : "50c7e7c524074102875fe4b1f49ea61a",
+                      "profileName" : "dvitski",
+                      "signatureRequired" : true,
+                      "textures" : {
+                        "SKIN" : {
+                          "url" : "http://textures.minecraft.net/texture/88aac6c902ac544377285161afa1d689ded118edf8330618d65c4649607a0585",
+                          "metadata" : {
+                            "model" : "slim"
+                          }
+                        },
+                        "CAPE" : {
+                          "url" : "http://textures.minecraft.net/texture/1de21419009db483900da6298a1e6cbf9f1bc1523a0dcdc16263fab150693edd"
+                        }
+                      }
+                    }
+                """.trimIndent()
+            ),
+            "MMfw9jGE8SRxGUPtXPsiqMv03hlFjZN4rfzDbScZxuIS+ugYK/ARU8839AcRVytfJgah8FBp3/FzB5dmW/8mh3gPPtm8wp//qM6vVcSifKu31T123H+u3+xNiDS3Ox+/+Fnd5t4x96v59/QcYoXIqLM2bxtMyBDEqeevUD41N5taRFpAs/2oS/p9ER3yInZchfoDwbAa3Dz1vBa2EKKNYQdxJLg86THqa6jq/ccgisEmzwHlSYN8/EAY7fYlN89oEUqxljLudp+8l48pt+nAo8+IOkWOF+T1UujsweoO5mYONM3v5dmpYGICFPUDPztZH1heSixYTz6N0k4XXmajZ/f7QeKy4O+sTHM39lbfAoQA9k311IweRkIkZuwc3eip/UIX+R/VS3YfBMCDZVPzqTMWhNf6FMirUAFL0WvzVK9qu+qR1Hpei3HZU+35c1ibKWKFiG3eoWvzB9eOvQCAtSx9NGeigUHEkzsfuTKfVHO+4w/MMjrqt8Be0CiE0BClBWjxjp5DzS14udCz0QXvHWF5os9HAss9oIUExQ9HOPxR7ZyvXKWGDcZdnvQoPqw/iOVwxt1nmjroxl3Sz9xCxp+YRRX2kUb+gYVlYor5ZPy9rJtMGWPQyMJVGAq/7dY55qR0Hw6lK6sO/EF688NiHjrD44LSAl6uqaobG0IYKww="
+        )
+
+        fun encode(string: String): String {
+            return Base64.getEncoder().encodeToString(string.encodeToByteArray());
+        }
     }
 }
